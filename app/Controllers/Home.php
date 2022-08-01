422f98312a59eb36ef8ad679a721ae3da7af048a
aldiperdana <muhammad.aldi.perdana.putra@gmail.com>
2022-08-01 13:37:38 +0800

    Master

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -21,9 +21,9 @@ class Home extends BaseController
         $pdf->Cell(0,7,'DAFTAR PEGAWAI AYONGODING.COM',0,1,'C');
         $pdf->Cell(10,7,'',0,1);
         $pdf->SetFont('Arial','B',10);
-        $pdf->Cell(10,6,'No',1,0,'C');
-        $pdf->Cell(90,6,'Nama Pegawai',1,0,'C');
-        $pdf->Cell(120,6,'Alamat',1,0,'C');
+        $pdf->Cell(10,6,'ID',1,0,'C');
+        $pdf->Cell(90,6,'Nama',1,0,'C');
+        $pdf->Cell(120,6,'Email',1,0,'C');
         $pdf->Cell(40,6,'Telp',1,1,'C');
         $pdf->SetFont('Arial','',10);
         $pegawai = $this->db->table("tbl_students")->get()->getResult();
